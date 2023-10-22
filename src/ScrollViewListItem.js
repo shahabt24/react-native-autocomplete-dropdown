8bc2a16e15018af20590b76e7a1ced720762bab6
shahabt24 <shahab_t24@yahoo.com>
2023-10-22 19:58:38 +0330

    Update ScrollViewListItem.js

--- src/ScrollViewListItem.js
+++ src/ScrollViewListItem.js
@@ -1,22 +1,20 @@
 import React, { memo, useMemo } from 'react'
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import diacriticless from './diacriticless';
 
-export const ScrollViewListItem = memo(({ highlight, title, style, onPress, ignoreAccents, numberOfLines = 2 }) => {
+export const ScrollViewListItem = memo(({ highlight, title, style, onPress, numberOfLines = 2 }) => {
   const titleParts = useMemo(() => {
     let titleHighlighted = ''
     let titleStart = title
     let titleEnd = ''
 
-  if (typeof title === 'string' && title?.length > 0 && highlight?.length > 0) {
-      const highlightIn = ignoreAccents ? diacriticless(title?.toLowerCase()) : title?.toLowerCase()
-      const highlightWhat = ignoreAccents ? diacriticless(highlight?.toLowerCase()) : highlight?.toLowerCase()
-
-      const substrIndex = highlightIn?.indexOf(highlightWhat)
+    if (typeof title === 'string' && title.length > 0) {
+      const substrIndex = title.toLowerCase().indexOf(highlight.toLowerCase())
       if (substrIndex !== -1) {
-        titleStart = title?.slice(0, substrIndex)
-        titleHighlighted = title?.slice(substrIndex, substrIndex + highlight?.length)
-        titleEnd = title?.slice(substrIndex + highlight?.length)
+        titleStart = title.slice(0, substrIndex)
+        titleHighlighted = title.slice(substrIndex, substrIndex + highlight.length)
+        titleEnd = title.slice(substrIndex + highlight.length)
+      } else {
+        titleStart = title
       }
     }
 
@@ -24,21 +22,21 @@ export const ScrollViewListItem = memo(({ highlight, title, style, onPress, igno
   }, [title, highlight])
 
   return (
-    <TouchableOpacity onPress={onPress}>
-      <View style={styles.container}>
-        <Text numberOfLines={numberOfLines}>
-          <Text numberOfLines={1} style={{ ...styles.text, ...style }}>
-            {titleParts.titleStart}
+      <TouchableOpacity onPress={onPress}>
+        <View style={styles.container}>
+          <Text numberOfLines={numberOfLines}>
+            <Text numberOfLines={1} style={{ ...styles.text, ...style }}>
+              {titleParts.titleStart}
+            </Text>
+            <Text numberOfLines={1} style={{ ...styles.text, ...style, fontWeight: 'bold' }}>
+              {titleParts.titleHighlighted}
+            </Text>
+            <Text numberOfLines={1} style={{ ...styles.text, ...style }}>
+              {titleParts.titleEnd}
+            </Text>
           </Text>
-          <Text numberOfLines={1} style={{ ...styles.text, ...style, fontWeight: 'bold' }}>
-            {titleParts.titleHighlighted}
-          </Text>
-          <Text numberOfLines={1} style={{ ...styles.text, ...style }}>
-            {titleParts.titleEnd}
-          </Text>
-        </Text>
-      </View>
-    </TouchableOpacity>
+        </View>
+      </TouchableOpacity>
   )
 })
 
